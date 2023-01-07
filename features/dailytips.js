2734a8ba74d1fc8afab2bc28b6846db796016fde
HenBOMB <alan.io.nightcode@gmail.com>
2023-01-07 19:19:48 -0300

    bugfix

--- features/dailytips.js
+++ features/dailytips.js
@@ -14,8 +14,8 @@ class DailyTips
     {
         process.conn.query(`SELECT tip_page, tip_index FROM sherbot WHERE id = '${process.guild.id}'`, async (err, res) => {
             process.logError(err);
-            page = res[0].dt_page;
-            tipIndex = res[0].dt_index;
+            page = res[0].tip_page;
+            tipIndex = res[0].tip_index;
             channel = process.guild.channels.cache.get('740552730250313809'); 
     
             const rule = new RecurrenceRule();
@@ -42,7 +42,7 @@ class DailyTips
     
                 res.on('data', chunk => {
                     data += chunk;
-                })
+                });
     
                 res.on('end', () => {
                     data = data.slice(data.indexOf(`post"`) + 5);
@@ -83,6 +83,45 @@ class DailyTips
         });
 
         process.conn.query(`UPDATE sherbot SET tip_page = ${page}, tip_index = ${tipIndex} WHERE id = 670107546480017409`, process.logError);
+    }
+
+    static preview(channelId)
+    {
+        process.conn.query(`SELECT tip_page, tip_index FROM sherbot WHERE id = '${process.guild.id}'`, async (err, res) => {
+            const page = res[0].tip_page;
+            const tipIndex = res[0].tip_index;
+            const channel = process.guild.channels.cache.get(channelId); 
+            const embed = new EmbedBuilder().setColor(process.botColor);
+            const imgUrls = [];
+
+            get(dtips_url + (178 - page), (res) => 
+            {
+                let data = "";
+    
+                res.on('data', chunk => {
+                    data += chunk;
+                });
+    
+                res.on('end', () => {
+                    data = data.slice(data.indexOf(`post"`) + 5);
+
+                    while (data.includes("_500")) 
+                    {
+                        const index = data.indexOf(`_500`);
+                        let url = data.slice(index-80, index+8);
+
+                        url = url.slice(url.indexOf("c=") + 3);
+                        data = data.slice(index + 20);
+
+                        imgUrls.push(url);
+                    }
+
+                    embed.setImage(imgUrls[imgUrls.length - tipIndex - 1]);
+                    
+                    channel.send({ embeds: [embed] });
+                });
+            });
+        });
     }
 }
 
